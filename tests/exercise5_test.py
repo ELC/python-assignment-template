58218348f785f1942feb02023acf8a66fd23e404
Ezequiel Leonardo Castaño <elcg@gmx.com>
2023-01-30 00:59:41 -0300

    Update Exercise5 Tests

--- tests/exercise5_test.py
+++ tests/exercise5_test.py
@@ -1,4 +1,4 @@
-"""Test Suite for Exercise 4"""
+"""Test Suite for Exercise 5"""
 
 # Traditional Assertion Test
 
@@ -6,23 +6,30 @@ import pytest
 from exercises import exercise5
 
 
-def test_division_assert_a_greater_than_b():
-    assert exercise5.division(10, 2) == 5
+@pytest.mark.parametrize(
+    "a, b, expected",
+    [
+        (10, 2, 5),
+        (10, -2, -5),
+        (-10, 2, -5),
+        (-10, -10, 1),
+        (0, 2, 0),
+        (0, -2, 0),
+    ],
+    ids=[
+        "a>0, b>0",
+        "a>0, b<0",
+        "a<0, b>0",
+        "a<0, b<0",
+        "a=0, b>0",
+        "a=0, b<0"
+    ],
+)
+def test_division_concrete_examples(a: float, b: float, expected: float):
+    assert exercise5.division(a, b) == expected
 
 
-def test_division_assert_a_less_than_b():
-    assert exercise5.division(-10, 2) == -5
-
-
-def test_division_assert_a_equals_b():
-    assert exercise5.division(-10, -10) == 1
-
-
-def test_division_assert_a_equals_zero():
-    assert exercise5.division(0, 2) == 0
-
-
-def test_division_assert_b_equal_zero():
+def test_division_denominator_equal_zero():
     with pytest.raises(ValueError):
         exercise5.division(1, 0)
 
@@ -33,35 +40,36 @@ from hypothesis import assume, given, strategies as st  # noqa: E402
 from exercises import exercise5  # noqa: E402, F811
 import math  # noqa: E402
 
+finate_floats = st.floats(allow_nan=False, allow_infinity=False)
 
-@given(st.floats(allow_nan=False, allow_infinity=False),
-       st.floats(allow_nan=False, allow_infinity=False))
+
+@given(finate_floats, finate_floats)
 def test_division_property_inverse(a, b):
     assume(b != 0)
 
     result = exercise5.division(a, b) * b
     assume(math.isfinite(result))
-    assert math.isclose(result, a)
+    assert math.isclose(result, a, abs_tol=1e-5), result
 
 
-@given(st.floats(allow_nan=False, allow_infinity=False))
+@given(finate_floats)
 def test_division_property_reverse(a):
     assume(a != 0)
     assert exercise5.division(a, a) == 1
 
 
-@given(st.floats(allow_nan=False, allow_infinity=False))
+@given(finate_floats)
 def test_division_property_identity_element(a):
     assert exercise5.division(a, 1) == a
 
 
-@given(st.floats(allow_nan=False, allow_infinity=False))
+@given(finate_floats)
 def test_division_property_absorbent_element(a):
     assume(a != 0)
     assert exercise5.division(0, a) == 0
 
 
-@given(st.floats(allow_nan=False, allow_infinity=False))
+@given(finate_floats)
 def test_division_property_nondefined(a):
     with pytest.raises(ValueError):
         exercise5.division(a, 0)
